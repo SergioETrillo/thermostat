868e444f6a526ff166bc08f5650bffbc5d7b2527
MariaRomero <fantasysystems@gmail.com>
2016-05-11 16:14:40 +0100

    thermostat app finished

--- spec/feature/thermostatSpec.js
+++ spec/feature/thermostatSpec.js
@@ -8,12 +8,31 @@ var thermostat;
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20)
+    expect(thermostat.temperature()).toEqual(20)
+  });
+
+  it('starts with power mode on', function() {
+    expect(thermostat.powerMode()).toBe(true)
+  });
+
+  it('I can switch power mode from on to off', function() {
+    expect(thermostat.switchPowerMode()).toBe(false)
+  });
+
+  it('when power mode on maximum temperature is 25', function() {
+    thermostat.up(10);
+    expect(thermostat.temperature()).toEqual(25)
+  });
+
+  it('when power mode off maximum temperature is 32', function() {
+    thermostat.switchPowerMode();
+    thermostat.up(15);
+    expect(thermostat.temperature()).toEqual(32)
   });
 
   it('temperature increases with up button', function() {
-    thermostat.up(10);
-    expect(thermostat.temperature).toEqual(30);
+    thermostat.up(5);
+    expect(thermostat.temperature()).toEqual(25);
   });
 
   it('throws error when temperature is below 10', function() {
@@ -22,6 +41,27 @@ var thermostat;
 
   it('temperature decreases with down button', function() {
     thermostat.down(5);
-    expect(thermostat.temperature).toEqual(15);
+    expect(thermostat.temperature()).toEqual(15);
   });
+
+  it('I can reset the temperature with the reset button', function() {
+    thermostat.reset();
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+  it('displays temperature in green if temperature < 18', function() {
+    thermostat.down(5);
+    expect(thermostat.colour()).toEqual('green');
+  });
+
+  it('displays temperature in yellow if temperature < 25', function() {
+    thermostat.up(4);
+    expect(thermostat.colour()).toEqual('yellow');
+  });
+
+  it('displays temperature in red if temperature 25 or more ', function() {
+    thermostat.up(6);
+    expect(thermostat.colour()).toEqual('red');
+  });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,17 +1,59 @@
+'use strict';
+
 function Thermostat() {
-  this.temperature = 20;
+  this._temperature = 20;
+  this._MINIMUM_TEMP = 10;
+  this._power_mode = true;
+  this._MAXIMUM_TEMP = 25;
+  this._MAXIMUM_TEMP_OFF = 32;
+  this._MAXIMUM_TEMP_ON = 25;
+  this._DEFAULT_TEMP = 20;
+  this._colour;
 }
+
+  Thermostat.prototype.powerMode = function() {
+    return this._power_mode;
+  };
+
+  Thermostat.prototype.temperature = function() {
+    return this._temperature;
+  };
+
+  Thermostat.prototype.switchPowerMode = function() {
+    this._power_mode = !this._power_mode;
+    if (this._power_mode) {
+      this._MAXIMUM_TEMP = this._MAXIMUM_TEMP_ON;
+    } else {
+      this._MAXIMUM_TEMP = this._MAXIMUM_TEMP_OFF;
+    }
+    return this._power_mode;
+  };
+
   Thermostat.prototype.up = function(number) {
-    return this.temperature = this.temperature + number;
-  }
+    if (this._temperature + number < this._MAXIMUM_TEMP)  {
+      return this._temperature = this._temperature + number;
+    }
+    return this._temperature = this._MAXIMUM_TEMP;
+  };
 
   Thermostat.prototype.down = function(number) {
-    if (this.temperature - number < 10) {
+    if (this._temperature - number < this._MINIMUM_TEMP) {
       throw new Error ('Minimum temperature has been reached');
     } else {
-      return this.temperature = this.temperature - number;
+      return this._temperature = this._temperature - number;
     }
-  }
-  // currentTemperature = function() {
-  //   return this.temperature;
-  // }
+  };
+
+  Thermostat.prototype.reset = function() {
+    this._temperature = this._DEFAULT_TEMP;
+  };
+
+  Thermostat.prototype.colour = function() {
+    if (this._temperature < 18) {
+      return this._colour = 'green';
+    } else if (this._temperature < 25) {
+      return this._colour = 'yellow';
+    } else {
+      return this._colour = 'red';
+    }
+  };
